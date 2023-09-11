5a45a82fa82ff11c1171fd0d3e2e34b32b5636e2
hyowon3434 <hyowon3434@naver.com>
2023-09-11 16:31:02 +0900

    first commit

---
+++ src/main/java/com/example/gnb/GnbApplication.java
@@ -0,0 +1,13 @@
+package com.example.gnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gnb/GnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
