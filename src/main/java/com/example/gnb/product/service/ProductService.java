abcd8d366a164baead40066e7c26353702f01f4f
Hyowon Kang <hyowon3434@naver.com>
2023-09-12 13:42:11 +0900

    Feat: create Product controller, service, repository

---
+++ src/main/java/com/example/gnb/product/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.gnb.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductService {
+
+}
