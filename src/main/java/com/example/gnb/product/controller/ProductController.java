83ac1f79cf5ad9a5a2e053f6ced12fa0ed469584
Hyowon Kang <hyowon3434@naver.com>
2023-09-12 16:31:54 +0900

    fix: builder를 찾지 못하는 에러 해결

--- src/main/java/com/example/gnb/product/controller/ProductController.java
+++ src/main/java/com/example/gnb/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.gnb.product.dto.CreateProductRequest;
 import com.example.gnb.product.entity.Product;
 import com.example.gnb.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class ProductController {
     private final ProductService productService;
 
     // 상품정보 등록
+    @PostMapping
     public Product createProduct(@RequestBody CreateProductRequest request){
         return productService.createProduct(request);
     }
--- src/main/java/com/example/gnb/product/service/ProductService.java
+++ src/main/java/com/example/gnb/product/service/ProductService.java
@@ -7,9 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-import static com.example.gnb.product.entity.Product.builder;
 
 @Service
 @RequiredArgsConstructor
