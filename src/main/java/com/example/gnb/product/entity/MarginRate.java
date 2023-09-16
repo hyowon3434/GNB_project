fd4d9d32b2c4e53a8ebb257491d576ec30496f25
Hyowon Kang <hyowon3434@naver.com>
2023-09-16 22:48:10 +0900

    Feat: create Margin_rate Entity

---
+++ src/main/java/com/example/gnb/product/entity/MarginRate.java
@@ -0,0 +1,36 @@
+package com.example.gnb.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "margin_rate")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MarginRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long margin_id;
+    @Setter
+    @Column
+    private int vat_invoice;
+    @Setter
+    @Column
+    private int tax_service_payment;
+    @Setter
+    @Column
+    private int net_profit;
+    @Setter
+    @Column
+    private float margin_rate;
+
+    @Builder
+    public MarginRate(int vat_invoice, int tax_service_payment, int net_profit, float margin_rate){
+        this.vat_invoice = vat_invoice;
+        this.tax_service_payment = tax_service_payment;
+        this.net_profit = net_profit;
+        this.margin_rate = margin_rate;
+
+    }
+}
