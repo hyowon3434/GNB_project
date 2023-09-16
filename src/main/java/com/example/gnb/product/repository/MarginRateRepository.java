098e0b97c8f8453f7d6db5238d4413374a80176c
Hyowon Kang <hyowon3434@naver.com>
2023-09-16 22:50:07 +0900

    Feat: create marginrate repository

---
+++ src/main/java/com/example/gnb/product/repository/MarginRateRepository.java
@@ -0,0 +1,8 @@
+package com.example.gnb.product.repository;
+
+import com.example.gnb.product.entity.MarginRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarginRateRepository extends JpaRepository<MarginRate, Long> {
+    
+}
