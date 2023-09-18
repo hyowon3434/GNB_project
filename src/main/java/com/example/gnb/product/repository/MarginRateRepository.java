d74541ec066011d646bdbfe79cdf2b2cd4bfb1c9
Hyowon Kang <hyowon3434@naver.com>
2023-09-18 13:46:09 +0900

    Fix: margin_rate 테이블 삭제

--- src/main/java/com/example/gnb/product/repository/MarginRateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gnb.product.repository;
-
-import com.example.gnb.product.entity.MarginRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarginRateRepository extends JpaRepository<MarginRate, Long> {
-    
-}
