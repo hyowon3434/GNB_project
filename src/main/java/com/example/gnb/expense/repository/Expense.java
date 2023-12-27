7a2431aa0c3e02ef2e7c7ead8a8ca34d11717dfb
Hyowon Kang <hyowon3434@naver.com>
2023-12-27 10:02:02 +0900

    Feat: created Expense Entity

---
+++ src/main/java/com/example/gnb/expense/repository/Expense.java
@@ -0,0 +1,36 @@
+package com.example.gnb.expense.repository;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "expense")
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long expenseId;
+    private String expenseType;
+    private String usage_content;
+    private Timestamp usedAt;
+    private Long usagePrice;
+    private  String expenseMemo;
+
+    @Builder
+    public Expense(Long expenseId, String expenseType, String usage_content,
+                   Timestamp usedAt, Long usagePrice, String expenseMemo){
+        this.expenseId = expenseId;
+        this.expenseType = expenseType;
+        this.usage_content = usage_content;
+        this.usedAt = usedAt;
+        this.usagePrice = usagePrice;
+        this.expenseMemo = expenseMemo;
+    }
+
+
+
+}
