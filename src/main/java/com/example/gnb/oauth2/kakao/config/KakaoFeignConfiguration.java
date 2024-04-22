94ce477fc53bb2c6c3b94ce2653753865eb10e1f
Hyowon Kang <hyowon3434@naver.com>
2024-04-22 23:43:24 +0900

    Feat: kakao config and Client class file

---
+++ src/main/java/com/example/gnb/oauth2/kakao/config/KakaoFeignConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.gnb.oauth2.kakao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class KakaoFeignConfiguration {
+
+
+}
