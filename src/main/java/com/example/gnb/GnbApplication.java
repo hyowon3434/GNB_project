4e07dcd9e4fc90bf4cf980394c79e5e3918574c2
Hyowon Kang <hyowon3434@naver.com>
2024-05-14 20:57:48 +0900

    Feat: added annotation to use open feign client

--- src/main/java/com/example/gnb/GnbApplication.java
+++ src/main/java/com/example/gnb/GnbApplication.java
@@ -1,9 +1,14 @@
 package com.example.gnb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication
+@EnableFeignClients
 public class GnbApplication {
 
 	public static void main(String[] args) {
