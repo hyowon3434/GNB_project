d9c452513882d9edafd663f66183233f6043c74d
Hyowon Kang <hyowon3434@naver.com>
2024-05-14 20:59:26 +0900

    Feat: Kakao controller, config

--- src/main/java/com/example/gnb/oauth2/kakao/config/KakaoFeignConfiguration.java
+++ src/main/java/com/example/gnb/oauth2/kakao/config/KakaoFeignConfiguration.java
@@ -1,9 +1,15 @@
 package com.example.gnb.oauth2.kakao.config;
 
+import feign.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 @Configuration
 public class KakaoFeignConfiguration {
 
-
+    @Bean
+    public Client fiegnClient(){
+        return new Client.Default(null, null);
+    }
 }
