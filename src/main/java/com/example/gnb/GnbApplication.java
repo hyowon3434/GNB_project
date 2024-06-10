eb0382b114a5ece97a23af2a547d2c538347a233
hyowon3434 <hyowon3434@naver.com>
2024-06-10 23:18:12 +0900

    Fix: deleted feign client annotations

--- src/main/java/com/example/gnb/GnbApplication.java
+++ src/main/java/com/example/gnb/GnbApplication.java
@@ -1,14 +1,9 @@
 package com.example.gnb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication
-@EnableFeignClients
 public class GnbApplication {
 
 	public static void main(String[] args) {
