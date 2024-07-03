336a78f6683a460e0a44d0604a16efdb5e134696
Hyowon Kang <hyowon3434@naver.com>
2024-07-03 21:59:38 +0900

    Feat: jwt token 생성 클래스, jwt 인증 필터 클래스, jwt 인증 resp9onse 클래스 구현

---
+++ src/main/java/com/example/gnb/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,60 @@
+package com.example.gnb.config.jwt;
+
+import com.example.gnb.config.auth.PrincipalDetails;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret_key}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication) {
+        PrincipalDetails userPrincipal = (PrincipalDetails) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUser().getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserEmailFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            // 로그 처리
+        } catch (MalformedJwtException ex) {
+            // 로그 처리
+        } catch (ExpiredJwtException ex) {
+            // 로그 처리
+        } catch (UnsupportedJwtException ex) {
+            // 로그 처리
+        } catch (IllegalArgumentException ex) {
+            // 로그 처리
+        }
+        return false;
+    }
+}
