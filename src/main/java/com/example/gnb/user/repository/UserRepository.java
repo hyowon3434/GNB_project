5e8f593da4527e12796fa0f6f3d6d6d93e7931fa
Hyowon Kang <hyowon3434@naver.com>
2024-07-03 22:03:05 +0900

    Feat: userService(회원가입 비즈니스 로직), userRepository 클래스 구현

--- src/main/java/com/example/gnb/user/repository/UserRepository.java
+++ src/main/java/com/example/gnb/user/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(Long userId);
     User findByUserName(String userName);
+    User findByEmail(String email);
 }
