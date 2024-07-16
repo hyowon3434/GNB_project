97fc970e15220b78496e8b09e9a505a33016caa4
hyowon3434 <hyowon3434@naver.com>
2024-07-16 22:11:40 +0900

    Feat: 구독 기능 구현

--- src/main/java/com/example/gnb/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/example/gnb/subscription/repository/SubscriptionRepository.java
@@ -1,2 +1,9 @@
-package com.example.gnb.subscription.repository;public interface SubscriptionRepository {
+package com.example.gnb.subscription.repository;
+
+import com.example.gnb.subscription.entity.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 }
